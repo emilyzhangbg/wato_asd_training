52a0fa8cd12edb0d37fd3d4a3cdb5ee6dc571813
Varun <v3mathur@uwaterloo.ca>
2025-01-17 22:40:49 -0500

    Added the first version of map_memory node

--- src/robot/map_memory/include/map_memory_node.hpp
+++ src/robot/map_memory/include/map_memory_node.hpp
@@ -2,7 +2,7 @@
 #define MAP_MEMORY_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-
+#include "nav_msgs/msg/Path.hpp"
 #include "map_memory_core.hpp"
 
 class MapMemoryNode : public rclcpp::Node {
@@ -11,6 +11,12 @@ class MapMemoryNode : public rclcpp::Node {
 
   private:
     robot::MapMemoryCore map_memory_;
+
+    // Constructs for ROS
+    rclcpp::Subscription<nav_msgs::OccupancyGrid>:: SharedPtr costmap_sub_;
+    rclcpp::Subscription<nav_msgs::Odomentry>:: SharedPtr odom_sub_;
+    rclcpp::Publisher<nav_msgs::OccupancyGrid>::SharedPtr map_pub_;
+    rclcpp::TimerBase::SharedPtr timer_;
 };
 
 #endif 
