27486154e9379136a928a2bc9dab7c1b6f5cb122
LSpinoti <spinotil@gmail.com>
2025-01-19 04:25:23 -0500

    Created template for ControlCore

--- src/robot/control/src/control_core.cpp
+++ src/robot/control/src/control_core.cpp
@@ -3,7 +3,18 @@
 namespace robot
 {
 
-ControlCore::ControlCore(const rclcpp::Logger& logger) 
-  : logger_(logger) {}
+ControlCore::ControlCore(const rclcpp::Logger& logger) : logger_(logger) {
+  lookahead_dist_ = 0.5;
+  goal_tolerance_ = 0.1;
+  linear_speed_ = 0.4;
+}
 
-}  
+
+void ControlCore::move(rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub_) {
+  auto delta = geometry_msgs::msg::Twist();
+  delta.linear.x = linear_speed_;
+
+  
+}
+
+}
--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -1,6 +1,28 @@
 #include "control_node.hpp"
 
-ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {}
+using std::placeholders::_1;
+
+ControlNode::ControlNode(): Node("control"), control_(robot::ControlCore(this->get_logger())) {
+  path_sub_ = this->create_subscription<nav_msgs::msg::Path>("/path", 10,
+                          std::bind(&ControlNode::recvPath, this, _1));
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("/odom/filtered", 10,
+                          std::bind(&ControlNode::recvOdom, this, _1));
+  twist_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
+  timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
+                          std::bind(&ControlNode::timerCallback, this));
+}
+
+void ControlNode::recvPath(nav_msgs::msg::Path::SharedPtr msg) {
+  control_.setPath(msg);
+}
+
+void ControlNode::recvOdom(nav_msgs::msg::Odometry::SharedPtr msg) {
+  control_.setOdom(msg);
+}
+
+void ControlNode::timerCallback(){
+  control_.move(twist_pub_);
+}
 
 int main(int argc, char ** argv)
 {
@@ -9,3 +31,4 @@ int main(int argc, char ** argv)
   rclcpp::shutdown();
   return 0;
 }
+
